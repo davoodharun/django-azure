8ba92f2bc24d279b8ae361817dae5cdd3c85dbae
Harun Davood <davoodharun>
2017-04-13 07:15:05 +0000

    'init'

---
+++ FounderTherapy/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\davoodharun\documents\visual studio 2015\Projects\FounderTherapy\FounderTherapy\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
